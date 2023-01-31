fd8cde5b3c452d35499e65095fc6610614ff2347
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-01-31 21:53:15 +0530

    header add

--- src/App.js
+++ src/App.js
@@ -6,26 +6,14 @@ import { useGlobalContext } from './States/Context/Context';
 import Header from './Components/Header/Header';
 
 
+
 function App() {
   const {modalhandler} = useGlobalContext()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Modal ModalContent={<Header></Header>} ClassName="new" ></Modal>
-        <button onClick={modalhandler}>modalhandler</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-body'>
+      <Header></Header>
+    </div>
     </div>
   );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import './Header.scss'
+import {MdShoppingCart  } from "react-icons/md";
 const Header = () => {
   return (
-    <header className='Header'></header>
+    <header className='Header'>
+      <div className="headerWrapper">
+      <div className="logo">Kishor Food Shop</div>
+      <div className="cart">
+      <span className='text'>Your Cart</span>  <MdShoppingCart></MdShoppingCart>
+         <span className='count'>2</span>
+      </div>
+      </div>
+    </header>
   )
 }
 
